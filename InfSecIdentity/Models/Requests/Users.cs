70ce38a039497798ed4c07db8637f37eea2c1074
tikasatryan <tikasatryan@gmail.com>
2022-08-29 19:06:47 +0400

    add
    add requests

---
+++ InfSecIdentity/Models/Requests/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace InfSecIdentity.Models.Requests
+{
+    public class Users
+    {
+        public Users()
+        {
+            public string Username { get; set; } = string.Empty;
+        }
+    }
+}
+
